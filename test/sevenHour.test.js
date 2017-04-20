994e3281b53180d7317a9feae3914c27f70feecd
Peter Lyons <pete@peterlyons.com>
2017-04-19 20:09:54 -0600

    Fixes to get tests passing but just surface stuff

--- test/sevenHour.test.js
+++ test/sevenHour.test.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import {shallow, mount} from "enzyme";
 import WeatherAPI from "./WeatherAPI.js";
 import Main from "../lib/components/Main.js";
-import sevenHour from "../lib/components/sevenHour.js";
+import SevenHour from "../lib/components/SevenHour.js";
 import Input from "../lib/components/Input.js";
 
-describe('sevenHour', ()=>{
+describe("sevenHour", () => {
+  it("should change when input is changed", () => {
+    let wrapper = shallow(<SevenHour sevenHour={[WeatherAPI]} />);
 
-it('should change when input is changed', () => {
-  let wrapper = shallow(<SevenHour sevenHourWeather={[WeatherAPI]} />);
-
-  // it("has a section with a current location", () => {
-  //   let currentLocation = wrapper.find(".current-container");
-  //
-  //   expect(currentLocation.length).toEqual(1);
-  // });
-  //
-  // it("displays the location of the user input", () => {
-  //   let currentLocation = wrapper.find(".current-location");
-  //
-  //   expect(currentLocation.props().children).toEqual("Denver, CO");
-  // });
+    // it("has a section with a current location", () => {
+    //   let currentLocation = wrapper.find(".current-container");
+    //
+    //   expect(currentLocation.length).toEqual(1);
+    // });
+    //
+    // it("displays the location of the user input", () => {
+    //   let currentLocation = wrapper.find(".current-location");
+    //
+    //   expect(currentLocation.props().children).toEqual("Denver, CO");
+    // });
+  });
 });
